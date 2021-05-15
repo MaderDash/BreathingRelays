c0acfd04bcf13aa69b49430525ca2caa8fd4d8af
Supperslash <32471649+Supperslash@users.noreply.github.com>
2021-05-14 21:24:06 -0500

    Update Main.ino

--- Main.ino
+++ Main.ino
@@ -12,7 +12,7 @@ int valvePins[5]    = {5, 6, 7, 8, 9};
 // Delay Array is how long to wait for each cycle
 // valules are in Milliseconds
 // example: 4000 Milliseconds = 4 seconds
-int DelayArray[5]   = {0, 1000, 4000, 450, 700};
+int DelayArray[8]   = {0, 4000, 450, 700, 4000, 450, 700, 1000};
 
 // Relay Triggers
 //===================
@@ -52,6 +52,7 @@ void setup() {
   for (int i = 0; i < 5; i++) {
     pinMode(valvePins[i], OUTPUT);
   }
+  delay(DelayArray[7]);
   Serial.println("Program Starting...");
   delay(DelayArray[0]);
   digitalWrite(9, HIGH);
@@ -62,12 +63,12 @@ void setup() {
 // Main Loop
 //===========
 void loop() {
-  cycler(1, 2);  // cycle 1, delay 2/
-  cycler(2, 3);  // cycle 2, delay 3/
-  cycler(3, 4);  // cycle 3, delay 4/
-  cycler(4, 2);  // cycle 4, delay 2/
-  cycler(5, 3);  // cycle 5, delay 3/
-  cycler(6, 4);  // cycle 6, delay 4/
+  cycler(1);  // cycle 1, 
+  cycler(2);  // cycle 2, 
+  cycler(3);  // cycle 3, 
+  cycler(4);  // cycle 4,
+  cycler(5);  // cycle 5, 
+  cycler(6);  // cycle 6, 
 }
 
 // Functions
@@ -76,15 +77,15 @@ void loop() {
 
 // Function: cycler
 //--------------------------
-// Cycler uses 2 parameters
+// Cycler uses 1 parameters
 // parameter 1 = cycle number
-// parameter 2 = points to which delay to use
-// for example DelayArray item 2 = 4000 Milliseconds = 4 Seconds
-void cycler(int call, int hold) {
+/* if you create more types of cycles add the 
+delay for it to the delayArray.*/
+void cycler(int call) {
   Serial.println(message[call]);
   for (int i = 0; i < 3; i++) {
-    int onOrOff=(triggerArray[call][i] == ON);
+    int onOrOff = (triggerArray[call][i] == ON);
     digitalWrite(valvePins[i], onOrOff);
   }
-  delay(DelayArray[hold]);
+  delay(DelayArray[call]);
 }
